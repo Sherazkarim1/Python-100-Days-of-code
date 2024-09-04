37fa8d63bfb0cea72fccdade0b1e1bb01711f708
sheraz <sherazkarim12@gmail.com>
2024-09-05 01:41:51 +0500

    print modifiers

--- Day 1/main.py
+++ Day 1/main.py
@@ -6,3 +6,10 @@ print("2. Knead the dough for 10 minutes.")
 print("3. Add 3g of Salt.")
 print("4. Leave to rise for 2 hours.")
 print("5. Bake at 200 degrees C for 30 minutes.")
+
+#Print modifiers
+
+print ("A 'single' quote inside double quote")
+print ('A "double" quote inside a single quote')
+print ("Alternatively you can just \"escape\" the quote")
+
